903328ec7e5b1e692356e0cfa52563ae9c10f7cd
KarolySzanto <KarolySzanto@e4354927-7f9f-4325-8ece-dc4325529103>
2012-05-12 09:54:19 +0000

    * updated marker icons
    git-svn-id: https://subversion.assembla.com/svn/itu_realms_project@44 e4354927-7f9f-4325-8ece-dc4325529103

--- trunk/realms_configurator/src/dk/itu/realms/web/map/RealmsMarker.java
+++ trunk/realms_configurator/src/dk/itu/realms/web/map/RealmsMarker.java
@@ -10,110 +10,112 @@ import dk.itu.realms.model.entity.Option;
 
 public class RealmsMarker extends Marker {
 
-	private static final String UNCOMPLETE_ICON 			= "/realms_configurator/faces/resources/images/marker_q.gif";
-	private static final String UNCOMPLETE_SELECTED_ICON	= "/realms_configurator/faces/resources/images/marker_q_selected.gif";
-	private static final String COMPLETE_ICON 				= "/realms_configurator/faces/resources/images/marker_done.png";
-	private static final String COMPLETE_SELECTED_ICON 		= "/realms_configurator/faces/resources/images/marker_done_selected.png";
-	
+	private static final String UNCOMPLETE_ICON 				= "/realms_configurator/faces/resources/images/marker_q.gif";
+	private static final String UNCOMPLETE_SELECTED_ICON		= "/realms_configurator/faces/resources/images/mmm.gif";
+	private static final String COMPLETE_Q_ICON 				= "/realms_configurator/faces/resources/images/marker_q.png";
+	private static final String COMPLETE_Q_SELECTED_ICON 		= "/realms_configurator/faces/resources/images/marker_q_selected.png";
+	private static final String COMPLETE_I_ICON 				= "/realms_configurator/faces/resources/images/marker_i.png";
+	private static final String COMPLETE_I_SELECTED_ICON 		= "/realms_configurator/faces/resources/images/marker_i_selected.png";
+
 	private RealmCircle accuracyCircle;
-	
+
 	/**
 	 * Constructor for a newly added marker 
 	 */
 	public RealmsMarker(LatLng latlng) {
 		this(new Mark(latlng.getLat(), latlng.getLng()));
 	}
-	
+
 	/**
 	 * Constructor for an existing marker 
 	 */
 	public RealmsMarker(Mark data) {
 		super(new LatLng(data.getLatitude(), data.getLongitude()));
-		
+
 		setData(data);
 		setDraggable(true);
-		
+
 		accuracyCircle = new RealmCircle(getLatlng(), data.getRadius(), this);
-		
+
 		updateIcon();
 	}
 
 	public RealmCircle getCircle() {
 		return accuracyCircle;
 	}
-	
+
 	/* data manipulation */
 	@Override
 	public void setLatlng(LatLng latlng) {
 		super.setLatlng(latlng);
-		
+
 		((Mark)getData()).setLatitude(latlng.getLat());
 		((Mark)getData()).setLongitude(latlng.getLng());
 
 		accuracyCircle.setCenter(latlng);
-		
+
 		updateIcon();
 	}
 	@Override
 	public LatLng getLatlng() {
 		return super.getLatlng();
 	}
-	
+
 	@Override
 	public void setTitle(String title) {
 		super.setTitle(title);
-		
+
 		((Mark)getData()).setMarkTitle(title);
-		
+
 		updateIcon();
 	}
 	@Override
 	public String getTitle() {
 		return ((Mark)getData()).getMarkTitle();
 	}
-	
+
 	public String getType() {
 		return ((Mark)getData()).getType();
 	}
 	public void setType(String type) {
 		((Mark)getData()).setType(type);
 	}
-	
+
 	public String getTextBlob() {
 		return ((Mark)getData()).getTextBlob();
 	}
 	public void setTextBlob(String textBlob) {
 		((Mark)getData()).setTextBlob(textBlob);
 	}
-	
+
 	public String[] getSupportedTypes() {
 		return ((Mark)getData()).getSupportedTypes();
 	}
-	
+
 	public boolean getIsInformation() {
 		return getType().equals("INFORMATION");
 	}
-	
+
 	public void setDescription(String description) {
 		((Mark)getData()).setMarkDescription(description);
-		
+
 		updateIcon();
 	}
 	public String getDescription() {
 		return ((Mark)getData()).getMarkDescription();
 	}
-	
+
 	public void setRadius(Double radius) {
 		((Mark)getData()).setRadius(radius);
-		
+
 		accuracyCircle.setRadius(radius);
-		
+
 		updateIcon();
 	}
 	public Double getRadius() {
 		return ((Mark)getData()).getRadius();
 	}
-	
+
 	public List<Option> getOptions() {
 		return ((Mark)getData()).getOptions();
 	}
@@ -125,27 +127,34 @@ public class RealmsMarker extends Marker {
 	public void setSelected(boolean selected) {
 		this.selected = selected;
 		accuracyCircle.setSelected(selected);
-		
+
 		updateIcon();
 	}
-	
+
 	public boolean isComplete() {
 		if(getData() == null || ! (getData() instanceof Mark)) {
 			return false;
 		}
-		
+
 		Mark markData = (Mark)getData();
-		
+
 		return markData.getMarkTitle() != null && !markData.getMarkTitle().isEmpty() &&
-			markData.getLatitude() != null && markData.getLongitude() != null;
+		markData.getLatitude() != null && markData.getLongitude() != null;
 	}
-	
+
 	private void updateIcon() {
 		if(isComplete()) {
 			if(isSelected()) {
-				setIcon(COMPLETE_SELECTED_ICON);
+				if(! getIsInformation()) {
+					setIcon(COMPLETE_Q_SELECTED_ICON);
+				} else {
+					setIcon(COMPLETE_I_SELECTED_ICON);
+				}
+
+			} else if(! getIsInformation()) {
+				setIcon(COMPLETE_Q_ICON);
 			} else {
-				setIcon(COMPLETE_ICON);
+				setIcon(COMPLETE_I_ICON);
 			}
 		} else {
 			if(isSelected()) {
